f975ab4c61d3912867525ba39174c2037a1be2e9
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-02-10 01:54:07 +0200

    Organizer

---
+++ ConsoleApp42/ConsoleApp42/Affairs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Affairs
+    {
+        public string title;
+        public bool isUrgentAffair;
+        public bool isImportant;
+        public Affairs(string title, bool isUrgentAffair, bool isImportant)
+        {
+            this.title = title; 
+            this.isUrgentAffair = isUrgentAffair;
+            this.isImportant = isImportant;
+        }
+
+        public void GetPriority()
+        {
+            Console.WriteLine("1");
+        }
+
+        public override string ToString()
+        {
+            
+            return $"This affair is: {this.title} {(isUrgentAffair ? "Urgant affair" : "Not urgant affair")} {(isImportant ? "Important affair" : "Not important affair")}";
+        }
+
+
+    }
+}
